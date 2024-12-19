416fe43c98b881309a641924e5a85f1333d5d066
paintoshi <88572775+paintoshi@users.noreply.github.com>
2024-12-19 08:18:48 +0000

    Deploying to gh-pages from @ PaintSwap/brush-bridge@3171095e4884f65dd93fa77dff68c317e96fd166 🚀

---
+++ _next/static/chunks/pages/registry-03f01da98e94f2b1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[841],{27618:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/registry",function(){return u(15296)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=27618)}),_N_E=n.O()}]);
